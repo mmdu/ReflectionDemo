16878a834b54b5ad9b46dfb696acda302aaafd03
mingming du <dafengdumingming@outlook.com>
2017-09-19 02:34:55 -0500

    Serialed Deserialed ,Stream

---
+++ SerializeDemo/SerializeDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerializeDemo
+{
+    [Serializable]
+    class A
+    {
+        public string test = "123456";
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = new A();
+            using (var stream = File.Open(typeof(A).Name + ".bin", FileMode.OpenOrCreate))
+            {
+                var bf = new BinaryFormatter();
+                bf.Serialize(stream, a);
+
+            }
+
+            A after = null;
+            using (var steam1 = File.Open(typeof(A).Name + ".bin", FileMode.Open))
+            {
+                var bf = new BinaryFormatter();
+                after = (A)bf.Deserialize(steam1);
+            }
+
+            Console.WriteLine(after.test);
+        }
+    }
+}
